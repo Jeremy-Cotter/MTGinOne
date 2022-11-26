2120871d530c71df13362840d5066609a6edb8b0
Jeremy-Cotter <koikesan120@gmail.com>
2022-11-25 22:17:19 -0500

    Initial Commit

---
+++ MTGinOne/MTGinOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGinOneApp.swift
+//  MTGinOne
+//
+//  Created by Jeremy  Cotter  on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MTGinOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
